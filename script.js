9f4f0cd7af7961f8238c5d2dc7060036416f4990
pedrobrustolingomes-art <pedro.brustolin.gomes@escola.pr.gov.br>
2026-08-25 12:08:14 -0300

    Add font size adjustment and theme toggle functions

---
+++ script.js
@@ -0,0 +1,44 @@
+let tamanhoFonte = 16;
+
+
+// AUMENTAR LETRA
+
+function aumentarFonte() {
+
+    if (tamanhoFonte < 24) {
+
+        tamanhoFonte += 2;
+
+        document.body.style.fontSize =
+            tamanhoFonte + "px";
+
+    }
+
+}
+
+
+// DIMINUIR LETRA
+
+function diminuirFonte() {
+
+    if (tamanhoFonte > 12) {
+
+        tamanhoFonte -= 2;
+
+        document.body.style.fontSize =
+            tamanhoFonte + "px";
+
+    }
+
+}
+
+
+// ==========================================
+// MODO CLARO / ESCURO
+// ==========================================
+
+function alternarTema() {
+
+    document.body.classList.toggle("escuro");
+
+}
